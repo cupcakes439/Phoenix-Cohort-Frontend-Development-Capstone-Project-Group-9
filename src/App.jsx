f608d0c7a40de036dda94584e85943c2adafc107
Adeola Akande <111195311+cupcakes439@users.noreply.github.com>
2026-05-28 10:44:34 +0100

    Add files via upload
    Initial react setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import PlanetSection from "./components/PlanetSection";
+import VideoSection from "./components/VideoSection";
+import ContactForm from "./components/ContactForm";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <VideoSection />
+      <PlanetSection />
+      <ContactForm />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
