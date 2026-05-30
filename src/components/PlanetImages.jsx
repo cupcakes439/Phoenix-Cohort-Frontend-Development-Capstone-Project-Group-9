00d8787aaeb99a6e70198b2f4d9270ecbc850866
okiezi wealth <okieziwealth@gmail.com>
2026-05-30 18:34:00 +0100

    Add planet images

--- src/components/PlanetImages.jsx
+++ src/components/PlanetImages.jsx
@@ -1,10 +1,42 @@
 import "../styles/planets.css";
+import { useState, useEffect } from 'react';
 
 function PlanetImages() {
+  const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://anurella.github.io/json/planets.json')
+      .then(res => res.json())
+      .then(data => {
+        setPlanets(data);
+        setLoading(false);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
+  if (loading) return <p>Loading planets...</p>;
+
   return (
-    <div>
-      Planet Images
-    </div>
+    <section id='planets'>
+      <div>
+        <h2>Visualizing the Differences Between Planets </h2>
+        <p>Each planet in our solar system has unique physical characteristics. visual comparisons help highlight how vastly different terrestial planets are from gas giants and ice giants .</p>
+        <div class="images">
+          {planets.map((planet) => (
+            <figure key={planet.planet}>
+              <img src={planet.image} alt={planet.planet} />
+              <figcaption>
+                <h3>{planet.planet}</h3>
+                <p>Distance from sun: {planet.distanceFromSun} AU</p>
+              </figcaption>
+            </figure>
+          ))}
+        </div>
+
+      </div>
+
+    </section>
   );
 }
 
