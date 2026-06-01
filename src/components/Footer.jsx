68a2d11b42306e3cb477094bb9e0bc461d5720c4
cupcakes439 <adeolaakande439@gmail.com>
2026-06-01 17:17:04 +0100

    Finished setting up to Footer.jsx file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,29 @@ import "../styles/footer.css";
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="footer">
+      <div className="footer-content">
+        <h3>About</h3>
+
+        <p className="members">
+          First name of group members
+        </p>
+
+        <hr />
+
+        <div className="footer-bottom">
+          <div className="copyright">
+            <p>©2026 Designed by Amaka & Ifeoma A.</p>
+            <p>Built by GroupName.</p>
+            <p>All rights reserved.</p>
+          </div>
+
+          <div className="academy">
+            <p>TSAcademy</p>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
 
