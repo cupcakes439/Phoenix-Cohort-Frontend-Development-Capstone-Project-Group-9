807c44e40047b58e9a9995355531749c9cd031ee
SpyTheCode <osemiyu@gmail.com>
2026-06-03 21:12:08 +0100

    feat: enhance Hero and Navbar components with new layout and styles
    - Updated Hero component to include a structured layout with a brief description and navigation buttons.
    - Redesigned Navbar component to feature a logo and improved styling.
    - Added new CSS styles for Hero and Navbar components to enhance visual presentation and responsiveness.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,42 @@ import "../styles/hero.css";
 
 function Hero() {
   return (
-    <div>
-      Hero
+    <div className="hero-section">
+      <div className="hero-content-wrapper">
+        <div className="hero-left-content">
+          <div className="page-brief">
+            <p>Explore Our Solar System Through Data</p>
+          </div>
+          <div className="page-description">
+            <p>
+              Understand the planets not just by name, but by measurable facts. From size and mass to gravity and
+              density, this page
+              breaks down the solar system in a clear, data-driven way.
+            </p>
+          </div>
+          <div className="button-frame">
+            <nav>
+              <ul>
+                <li>
+                  <a className="nav-button" href="#planets">
+                    Explore the Data
+                  </a>
+                </li>
+                <li>
+                  <a className="nav-button" href="#contactSection">
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div className="hero-right-content">
+          <figure>
+            <img className="plant-thumbnail" src="/images/075cfe8711f093d2ee4330cfa37d385003066f0a.png" alt="earth" />
+          </figure>
+        </div>
+      </div>
     </div>
   );
 }
