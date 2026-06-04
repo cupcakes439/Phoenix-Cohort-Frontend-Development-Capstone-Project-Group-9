d433e36ddd8d8ac70c665135a6376faa35327d91
cupcakes439 <adeolaakande439@gmail.com>
2026-06-04 13:03:08 +0100

    Done with the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,28 +4,61 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <h3>About</h3>
+       <h3>About</h3>
 
-        <p>First name of group members:</p>
-        <p className="members">Adeola • Olayinka • Semiyu • Ayobami • Abdulmuiz • Okiezi • Sunday • Victoria • Chidinma • Oluwanifemi • Victoria • Idowu • Anita</p>
+        
+       <p className="team-description">We are Group 9 members of the Capstone Project for Frontend Development Phoenix Cohort, collaborating to build a responsive and user-friendly React application while applying modern frontend development practices, teamwork, and version control workflows.</p>
 
-        <hr />
+       <p>First name of group members:</p>
+       <p className="members">Adeola • Olayinka • Semiyu • Ayobami • Abdulmuiz • Okiezi • Sunday • Victoria • Chidinma • Oluwanifemi • Idowu • Victoria • Anita</p>
 
-        <div className="footer-bottom">
-          <div className="copyright">
-            <p>&copy;2026 Designed by Amaka & Ifeoma A.</p>
-            <p className="footer-text">
-              Built by Phoenix-Cohort-Frontend-Development-Capstone-Project-Group-9. 
-              <span className="rights"> All rights reserved</span>
-            </p>
-          </div>
+       <hr />
 
-          <div className="academy">
-            <p>TSAcademy</p>
-          </div>
+       <div className="footer-bottom">
+         <div className="footer-info">
+           <p>&copy;2026 Design by{" "}
+           <a
+            href="https://amakandukwu.com"
+            target="_blank"
+            rel="noopener noreferrer"
+           >
+            Amaka
+           </a>
+           {" & "}
+           <a
+             href="https://www.linkedin.com/in/ifeomaokocha/"
+             target="_blank"
+             rel="noopener noreferrer"
+           >
+             Ifeoma A.
+           </a>
+         </p>
+
+         <p>
+          Built by{" "}
+           <a
+             href="https://github.com/cupcakes439/Phoenix-Cohort-Frontend-Development-Capstone-Project-Group-9.git"
+             target="_blank"
+             rel="noopener noreferrer"
+           >
+             Phoenix Cohort Frontend Development Capstone Project Group 9
+           </a>
+           . All rights reserved 
+         </p>
         </div>
+
+       <div className="footer-brand">
+         <a
+           href="https://tsacademyonline.com"
+           target="_blank"
+           rel="noopener noreferrer"
+         >
+           TSAcademy
+         </a>
+       </div>
       </div>
-    </footer>
+    </div>
+   </footer>
   );
 }
 
