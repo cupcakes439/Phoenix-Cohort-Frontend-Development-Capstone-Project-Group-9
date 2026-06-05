37f7f8afed6dc72f4732f20d78a27588d193d312
SpyTheCode <osemiyu@gmail.com>
2026-06-05 09:59:26 +0100

    Returned everything to default

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,9 @@ import "../styles/navbar.css";
 
 function Navbar() {
   return (
-    <header className="navbar-top">
-      <figure className="logo-fig-wrap">
-        <img className="logo-thumbnail" src="/images/graphics.svg" alt="logo" />
-      </figure>
-      <span className="left-side-cont"></span>
-    </header>
+    <div>
+      Navbar
+    </div>
   );
 }
 
