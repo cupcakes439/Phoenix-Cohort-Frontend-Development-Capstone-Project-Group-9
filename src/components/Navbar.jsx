6842e1e9f1daeb84a2cd7db496dd62ced772d083
SpyTheCode <osemiyu@gmail.com>
2026-06-05 10:45:08 +0100

    I completed the navbar section with the logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,12 @@ import "../styles/navbar.css";
 
 function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <header className="navbar-top">
+      <figure className="logo-fig-wrap">
+        <img className="logo-thumbnail" src="/images/graphics.svg" alt="logo" />
+      </figure>
+      <span className="left-side-cont"></span>
+    </header>
   );
 }
 
